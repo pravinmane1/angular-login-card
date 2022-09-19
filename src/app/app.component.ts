9faf2f118912e3de315206b86db5a7483b26d059
Pravin Baburao Mane <PMane4@slb.com>
2022-09-20 02:55:28 +0530

    Added validations and thank you page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'card-challenge';
+  public formSubmitted: boolean;
+
+  @ViewChild('f') cardForm: NgForm;
+
+  onSubmit(ngForm: NgForm) {
+    this.formSubmitted = true;
+  }
+
+  resetForm(){
+    this.formSubmitted = false;
+  }
+
+  onCardNumberEnter(inputEvent: any) {
+    let cardNumber = inputEvent.target.value;
+
+    const cardNumberWithoutSpace = cardNumber.replaceAll(' ', '');
+    let cardNumberWithSpace = '';
+
+    for (let i = 0; i < cardNumberWithoutSpace.length; i++) {
+      if (i % 4 === 0 && i !== 0) {
+        cardNumberWithSpace += ' ' + cardNumberWithoutSpace.charAt(i);
+      } else {
+        cardNumberWithSpace += cardNumberWithoutSpace.charAt(i);
+      }
+    }
+    this.cardForm.form.patchValue({ cardNumber: cardNumberWithSpace });
+  }
 }
